764cb0e1b505bdc33582fb2f13d7cede21954ad2
Pierre Keane <Pierrekeane@gmail.com>
2025-12-03 16:09:44 +0800

    corrected the link in navbar for dashboard, applied current user in the exp controller, added title to dashboard page.

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,8 +3,8 @@ class ExpensesController < ApplicationController
   def index
 
     if params[:country].present?
-      @trips = Trip.where(country: params[:country])
-      @expenses = Expense.where(trip: @trips)
+      @trips = Trip.where(user: current_user, country: params[:country])
+      @expenses = Expense.where(user: current_user, trip: @trips)
 
       # Expenses Pie Chart
       @expenses_by_category = @expenses.group(:category).sum("base_amount_cents / 100.0")
@@ -21,8 +21,8 @@ class ExpensesController < ApplicationController
       end
 
     else
-      @expenses = Expense.all
-      @trips = Trip.all
+      @expenses = Expense.all.where(user: current_user, )
+      @trips = Trip.all.where(user: current_user)
 
       # Expenses Pie Chart
       @expenses_by_category = @expenses.group(:category).sum("base_amount_cents / 100.0")
@@ -39,7 +39,7 @@ class ExpensesController < ApplicationController
       end
     end
 
-    @filter = Trip.all
+    @filter = Trip.all.where(user: current_user)
     @summary = summarize_exp
   end
 
@@ -47,8 +47,9 @@ class ExpensesController < ApplicationController
     @expense = Expense.new
     @trip = Trip.find(params[:trip_id])
 
-    @trips = Trip.all
-    @expenses = Expense.all
+    raise
+    @trips = Trip.all.where(user: current_user)
+    @expenses = Expense.all.where(user: current_user)
     @summary = summarize_exp
 
   end
